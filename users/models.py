c18d3fdff9ee6f90cbeffc9325b316fda58d8433
soash <soash2002@gmail.com>
2026-04-25 13:25:11 +0600

    commit

--- users/models.py
+++ users/models.py
@@ -42,10 +42,37 @@ class CustomUser(AbstractUser):
         (f"{year}-{year+1}", f"{year}-{year+1}") for year in range(datetime.now().year, 1989, -1)
     ]
 
+    HOMETOWN_CHOICES = [
+        ('', '-- Select District --'),
+        ('Bagerhat', 'Bagerhat'), ('Bandarban', 'Bandarban'), ('Barguna', 'Barguna'),
+        ('Barisal', 'Barisal'), ('Bhola', 'Bhola'), ('Bogura', 'Bogura'),
+        ('Brahmanbaria', 'Brahmanbaria'), ('Chandpur', 'Chandpur'), ('Chapainawabganj', 'Chapainawabganj'),
+        ('Chattogram', 'Chattogram'), ('Chuadanga', 'Chuadanga'), ('Comilla', 'Comilla'),
+        ('Coxs Bazar', "Cox's Bazar"), ('Dhaka', 'Dhaka'), ('Dinajpur', 'Dinajpur'),
+        ('Faridpur', 'Faridpur'), ('Feni', 'Feni'), ('Gaibandha', 'Gaibandha'),
+        ('Gazipur', 'Gazipur'), ('Gopalganj', 'Gopalganj'), ('Habiganj', 'Habiganj'),
+        ('Jamalpur', 'Jamalpur'), ('Jessore', 'Jessore'), ('Jhalokati', 'Jhalokati'),
+        ('Jhenaidah', 'Jhenaidah'), ('Joypurhat', 'Joypurhat'), ('Khagrachhari', 'Khagrachhari'),
+        ('Khulna', 'Khulna'), ('Kishoreganj', 'Kishoreganj'), ('Kurigram', 'Kurigram'),
+        ('Kushtia', 'Kushtia'), ('Lakshmipur', 'Lakshmipur'), ('Lalmonirhat', 'Lalmonirhat'),
+        ('Madaripur', 'Madaripur'), ('Magura', 'Magura'), ('Manikganj', 'Manikganj'),
+        ('Meherpur', 'Meherpur'), ('Moulvibazar', 'Moulvibazar'), ('Munshiganj', 'Munshiganj'),
+        ('Mymensingh', 'Mymensingh'), ('Naogaon', 'Naogaon'), ('Narail', 'Narail'),
+        ('Narayanganj', 'Narayanganj'), ('Narsingdi', 'Narsingdi'), ('Natore', 'Natore'),
+        ('Netrokona', 'Netrokona'), ('Nilphamari', 'Nilphamari'), ('Noakhali', 'Noakhali'),
+        ('Pabna', 'Pabna'), ('Panchagarh', 'Panchagarh'), ('Patuakhali', 'Patuakhali'),
+        ('Pirojpur', 'Pirojpur'), ('Rajbari', 'Rajbari'), ('Rajshahi', 'Rajshahi'),
+        ('Rangamati', 'Rangamati'), ('Rangpur', 'Rangpur'), ('Satkhira', 'Satkhira'),
+        ('Shariatpur', 'Shariatpur'), ('Sherpur', 'Sherpur'), ('Sirajganj', 'Sirajganj'),
+        ('Sunamganj', 'Sunamganj'), ('Sylhet', 'Sylhet'), ('Tangail', 'Tangail'),
+        ('Thakurgaon', 'Thakurgaon'),
+    ]
+
     whatsapp_number = models.CharField(max_length=15, blank=True, null=True)
     social_profile = models.URLField(max_length=200, blank=True, null=True)
     registration_number = models.CharField(max_length=50, blank=True, null=True)
     department = models.ForeignKey(Department, on_delete=models.SET_NULL, blank=True, null=True)
+    hometown = models.CharField(max_length=50, choices=HOMETOWN_CHOICES, blank=True, null=True)
 
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
     blood = models.CharField(max_length=3, choices=BLOOD_GROUP_CHOICES, blank=True, null=True)
