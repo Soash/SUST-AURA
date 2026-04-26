d38c030effaa123326422110e824db85482e9faa
soash <soash2002@gmail.com>
2026-04-26 19:22:39 +0600

    commit

--- users/admin.py
+++ users/admin.py
@@ -6,6 +6,20 @@ from .models import CustomUser, Department, School, PrimarySetting
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
+    list_display = ['username', 'email', 'first_name', 'last_name', 'department', 'school', 'email_verified', 'is_staff']
+    list_filter = UserAdmin.list_filter + ('department', 'school', 'email_verified', 'gender', 'blood')
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ('Academic Info', {
+            'fields': ('registration_number', 'department', 'school', 'session', 'student_proof')
+        }),
+        ('Personal Info', {
+            'fields': ('gender', 'blood', 'hometown', 'whatsapp_number', 'social_profile')
+        }),
+        ('Status', {
+            'fields': ('email_verified',)
+        }),
+    )
 
 
 @admin.register(Department)
